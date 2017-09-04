58b5c79a27ac4579200c810056ea5185ba6582ce
Aashish Pandey <aashishkpandey@gmail.com>
2017-09-04 12:08:29 +0530

    try correction

--- global.R
+++ global.R
@@ -1,7 +1,7 @@
 max_plots = 20
 wc1 = function(n,mat1,mat2){
   for (i in 1:ncol(mat2)) {
-    freq = as.matrix(mat1[(match(rownames(mat2[(mat2[,i] > 1),]),rownames(mat1))),][,i])
+  freq = as.matrix(mat1[(match(rownames(mat2[(mat2[,i] > 1),]),rownames(mat1))),][,i])
     freq = as.matrix(freq[(order(freq[,1], decreasing=T)),])
     top_word = as.matrix(freq[1:n,])
     #mypath <- (paste("WC_mat2_theta_topic",i,"_",n, ".jpeg", sep = ""))
