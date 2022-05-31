d05ad5babf977334f40387e18631b7ef70ceba8f
Shivani Srivastava <shivani141296@gmail.com>
2022-05-31 09:11:36 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -59,8 +59,8 @@ shinyUI(fluidPage(
                          verbatimTextOutput("start"),
                          h4(p("Download Sample text file")), 
                          downloadButton('downloadData1', 'Download Nokia Lumia reviews txt file'),br(),br(),
-                         downloadButton('downloadData2', 'Download Uber Reviews CSV file'),br(),br(),
-                         downloadButton('downloadData3', 'Download Airline Tweets CSV file'),br(),br(),
+                         downloadButton('downloadData3', 'Download Uber Reviews CSV file'),br(),br(),
+                         downloadButton('downloadData4', 'Download Airline Tweets CSV file'),br(),br(),
                          p("Please note that download will not work with RStudio interface. Download will work only in web-browsers. So open this app in a web-browser and then download the example file. For opening this app in web-browser click on \"Open in Browser\" as shown below -"),
                          img(src = "example1.png")
                          ),
