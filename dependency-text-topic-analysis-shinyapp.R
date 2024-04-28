37be74274734fe2f922fffe19bc8c135addd5cc2
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-04-28 12:14:57 +0530

    Update dependency-text-topic-analysis-shinyapp.R

--- dependency-text-topic-analysis-shinyapp.R
+++ dependency-text-topic-analysis-shinyapp.R
@@ -1,5 +1,6 @@
 # Install / Load relevant packages
 if (!require(shiny)){install.packages("shiny"); library(shiny)}
+if (!require(pdftools)){install.packages("pdftools", quiet = TRUE)}; library(pdftools)
 if(!"pacman" %in% installed.packages()[,"Package"]) install.packages("pacman")
 pacman::p_load(shiny,text2vec, tm, tokenizers, wordcloud,slam,maptpx,igraph,DT,tools,stringr,dplyr)
 #pacman::p_load(DT)
